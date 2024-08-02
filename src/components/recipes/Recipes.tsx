3e910d410b914ecd273331c7b2fd9ad657dcd93c
NavyCuda <navysupra@gmail.com>
2024-08-02 15:06:01 -0700

    Get ingredients to list, very messy needs refactor

--- src/components/recipes/Recipes.tsx
+++ src/components/recipes/Recipes.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Input from '../common/Input';
+import { useInventory } from '../../state/uses/useInventory';
 
 
 
@@ -10,19 +11,38 @@ import Input from '../common/Input';
 
 
 export default function Recipes(props) {
-  const [ ingredients, setIngredients ] = useState([
-    getIngredient()
-  ]);
+  const [ inventoryItems, setInvetoryItems ] = useState([]);
 
-  function getIngredient(index:number = 0){
+
+  const inventory = useInventory();
+
+
+  
+  console.log('inventoryItems', inventoryItems);
+
+  console.log("Recipes", inventory);
+  const _inventoryItems = inventoryItems.map((item, itemIndex) => {
+    const key = `${item.name}_${itemIndex}`;
+    const { id, name, qty, unit } = item;
+    return (
+      <option key={key} value={id}>
+        {name}
+      </option>
+    )
+  })
+
+  console.log("_inventoryItems",_inventoryItems)
+
+  const getIngredient = (index:number = 0) => {
     const key = `ingredient_${index}`;
-    const qtyKey = `${key}_qty`;
+    const qtyKey = `ingredientQty_${index}`;
     const removeIngredient = () => {
       setIngredients(ingredients.filter((_,i)=>i!==index));
     }
     return (
       <div key={key}>
         <select name={key}>
+          {_inventoryItems}
         </select>
         <Input id={qtyKey} name='Qty?' type='number'/>
         <div
@@ -32,13 +52,46 @@ export default function Recipes(props) {
     )
   }
 
+  const [ ingredients, setIngredients ] = useState([]);
+
   const addIngredient = () => {
     setIngredients([...ingredients, getIngredient(ingredients.length)])
   }
 
   const handleOnSubmit = (e:React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("submit called");
+    const formData = new FormData(e.target);
+    const recipe:{[key:string]:unknown} = {
+      ingredients: []
+    };
+
+
+    const ingredients:{[index:number]:{id:string,qty:number}} = {};
+
+    formData.forEach((value,key) => {
+      const ingredientPattern = /ingredient(?<qty>Qty)?_(?<index>\d+)/i;
+      const keyMatch = key.match(ingredientPattern);
+      
+      if (keyMatch){
+        const { qty, index } = keyMatch.groups;
+        if (!ingredients[index]){
+          ingredients[index] = {
+            id: "",
+            qty: 0
+          } 
+        }
+        if (qty) {
+          ingredients[index].qty = parseInt(value as string)
+        } else {
+          ingredients[index].id = value as string
+        }
+      } else {
+        recipe[key] = value;
+      }
+    });
+    
+    recipe.ingredients = Object.values(ingredients);
+    console.log("submit called",recipe);
   }
 
 
@@ -48,6 +101,14 @@ export default function Recipes(props) {
     addIngredient();
   }
 
+
+  useEffect(() => {
+    setInvetoryItems(inventory.data);
+    if (!ingredients.length){
+      addIngredient();
+    }
+    console.log('Recipes.useEffect', inventory.data);
+  }, [inventory.data])
 
 
   return (
